960688e4764c36021ae2b4123086e48d500aa0d2
Fernando Rodríguez <frr@keepcoding.io>
2021-05-12 11:27:14 +0200

    removed useless stuff

--- lib/nobust.dart
+++ lib/nobust.dart
@@ -3,8 +3,5 @@
 /// More dartdocs go here.
 library nobust;
 
-export 'src/nobust_base.dart';
 export 'src/notification.dart';
 export 'src/notification_center.dart';
-
-// TODO: Export any libraries intended for clients of this package.
