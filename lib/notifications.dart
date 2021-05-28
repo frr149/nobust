4d3e4757dc99e06b932694f5d028f7563c8304f7
Fernando Rodríguez <frr@keepcoding.io>
2021-05-28 10:55:15 +0200

    changes removed to different library

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -3,7 +3,5 @@
 /// More dartdocs go here.
 library notifications;
 
-export 'src/changes/change_emitter.dart';
-export 'src/changes/change_receiver.dart';
 export 'src/notification.dart';
 export 'src/notification_center.dart';
